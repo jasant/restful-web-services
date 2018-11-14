539f47028a6a3211e61d078627f47fe729859eeb
Alberto <alberto.santillan@fonyou.com>
2018-11-13 19:21:03 -0600

    Step 03
    Hello world rest service example

---
+++ src/main/java/com/jasant/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.jasant.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/get-hello-world")
+	public String getHelloWorld() {
+		return "Hello World";
+	}
+	
+}
